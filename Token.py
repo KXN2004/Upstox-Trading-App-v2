69141276eeca713a5641afc598ea0ac5033a77fe
Kevin Nadar <kevinxaviernadar@student.sfit.ac.in>
2023-12-15 15:01:13 +0530

    Automated code entering process
    Waits for some time and automatically copies and pastes the generated code from the url in the address bar of the web browser

--- Token.py
+++ Token.py
@@ -1,31 +1,43 @@
-import json, getpass, webbrowser, upstox_client
+import time, json, pyperclip, pyautogui, platform, webbrowser, upstox_client
 from upstox_client.rest import ApiException
 
-# Launch the default web browser and ask the user to login
-webbrowser.get('chrome').open('https://api.upstox.com/v2/login/authorization/dialog?response_type=code&client_id=d6cb7427-c883-456f-9e2a-5f01f944fd78&redirect_uri=https://account.upstox.com/contact-info/')
+match platform.system():
+    case 'Windows':
+        modifier = 'ctrl'
+    case 'Darwin':
+        modifier = 'command'
 
-# Ask the user to enter the code
-code = getpass.getpass("Enter the code: ")
+with open('credentials.json', 'r') as file:
+    credentials = json.load(file)
+
+# Launch the default web browser and ask the user to login
+webbrowser.get('chrome').open(f'https://api.upstox.com/v2/login/authorization/dialog?response_type=code&client_id={credentials["client_id"]}&redirect_uri={credentials["redirect_url"]}')
+
+url = 'Waiting for authorization...'
+print(url)
+while not url.startswith(credentials['redirect_url'] + '?code='):
+    time.sleep(5)
+    pyautogui.hotkey(modifier, 'l')
+    pyautogui.hotkey(modifier, 'c')
+    url = pyperclip.paste()
+    code = url.split('=')[-1]
 
 # create an instance of the API class
 api_instance = upstox_client.LoginApi()
 
 try:
     # Get token API
-    # TODO: Use one single JSON file to store the client_id, client_secret, redirect_uri and grant_type
-    with open('credentials.json') as file:
-        credentials = json.load(file)
-        api_response = api_instance.token(
-            api_version='2.0',
-            code=code,
-            client_id=credentials['client_id'],
-            client_secret=credentials['client_secret'],
-            redirect_uri='https://account.upstox.com/contact-info/',
-            grant_type='authorization_code'
-        )
-        with open('credentials.json', 'w') as file:
-            credentials['access_token'] = api_response.access_token
-            json.dump(credentials, file)
+    api_response = api_instance.token(
+        api_version='2.0',
+        code=code,
+        client_id=credentials['client_id'],
+        client_secret=credentials['client_secret'],
+        redirect_uri=credentials['redirect_url'],
+        grant_type='authorization_code'
+    )
+    credentials['access_token'] = api_response.access_token
+    with open('credentials.json', 'w') as file:
+        json.dump(credentials, file)
     print("Generated the access token succesfully!")
 except ApiException as e:
     print("Exception when calling LoginApi->token: %s\n" % e)
